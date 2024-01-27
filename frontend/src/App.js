b87ad8fbb24bcdc185aa0de16c7b78252cfc4c3e
Dean Nguyen <deanpn@uci.edu>
2024-01-27 15:57:33 -0800

    Sign up Notification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import SignUpPage from './pages/SignUp';
 import App_profile from './pages/App_profile';
+import { ChakraBaseProvider } from '@chakra-ui/react';
 const App = () => {
   return (
-    <>
-    <Routes>
-      <Route index element={<SignUpPage />} />
-      <Route path="/Sign_Up" element={<SignUpPage />} />
-      <Route path="/Profile" element={<App_profile />} />
-    </Routes>
-    </>
+    <ChakraBaseProvider>
+      <Routes>
+        <Route index element={<SignUpPage />} />
+        <Route path="/Sign_Up" element={<SignUpPage />} />
+        <Route path="/Profile" element={<App_profile />} />
+      </Routes>
+    </ChakraBaseProvider>
   );
 };
 export default App;
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,21 +1,24 @@
 import React, { useState, useEffect} from 'react';
-import {ChakraProvider, Box, Heading, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button, Link as ChakraLink, Select } from '@chakra-ui/react';
+import {ChakraProvider, Alert, Box, Heading, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button, Link as ChakraLink, Select } from '@chakra-ui/react';
 import {getAPI, postAPI} from '../utils/util';
-
 
 
 const SignUpPage = () => {
   // Fields for the form
   const formFields = ['username', 'email', 'school_email', 'password', 'school_id'];
-
+  const codeConfig = {
+    "-1": "Failed Sign Up!",
+    "0": "Username is taken!",
+    "1": "Invalid school email!",
+    "2": "Successful Sign Up!"
+  };
   // stters and getters for school
   const [schools, setSchools] = useState([]);
   const [loading, setLoading] = useState(true);
   // Setter and getter for password visibility
   const [show, setShow] = useState(false);
-
   const [successCode, setSuccessCode] = useState(-1);
-
+  const [successVisibility, setSuccessVisibility] = useState(false);
   // Create dictionary for form fields
   const [formData, setFormData] = useState(
     Object.fromEntries(
@@ -45,14 +48,10 @@ const SignUpPage = () => {
     // Add your signup logic here
     formData["school_id"] = parseInt(formData["school_id"]);
     postUserInfo();
-    switch(successCode)
-    {
-      case -1:
-        
-      case 0:
-      case 1:
-      case 2:
-    }
+    setSuccessVisibility(true);
+    setTimeout(() => {
+      setSuccessVisibility(false);
+    }, 5000);
   };
 
   // API post to send user sign up info on Submit
@@ -79,8 +78,10 @@ const SignUpPage = () => {
     }}
   , []);
 
+
   return (
     <ChakraProvider>
+      {successVisibility && (<Alert justifyContent='center' textAlign='center' status={(successCode === 2) ? "success" : "error"} variant='subtle'>{codeConfig[successCode.toString()]}</Alert>)}
       <Box p={4} maxW="400px" mx="auto">
         <Heading as="h2" mb={6} textAlign="center">
           Sign Up
