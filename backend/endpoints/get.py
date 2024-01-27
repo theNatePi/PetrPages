aae39a68580ca87931158b0e3a21e21c673ec7f9
Nathan Pietrantonio <pietrann@uci.edu>
2024-01-26 23:24:17 -0800

    Implement /schools endpoint

--- backend/endpoints/get.py
+++ backend/endpoints/get.py
@@ -1,10 +1,17 @@
 from components import school
 from database import get_all_schools
 from api import app
+import json
 
 
 @app.get("/schools/")
 def get_schools():
     result = get_all_schools()
-    print(result)
+    return_json = {}
+    for school in result:
+        return_json[int(school[0])] = school[1]
+    
+    return_json = json.dumps(return_json)
+    print(return_json)
+    return {return_json}
 
