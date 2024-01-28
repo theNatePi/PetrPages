007206bc0941d5ef335d27686577eea10f6112ca
Dean Nguyen <deanpn@uci.edu>
2024-01-27 16:05:21 -0800

    Use grid to center sign up

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react';
-import {ChakraProvider, Alert, Box, Heading, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button, Link as ChakraLink, Select } from '@chakra-ui/react';
+import {ChakraProvider, Grid, Alert, Box, Heading, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button, Link as ChakraLink, Select } from '@chakra-ui/react';
 import {getAPI, postAPI} from '../utils/util';
 
 
@@ -82,65 +82,88 @@ const SignUpPage = () => {
   return (
     <ChakraProvider>
       {successVisibility && (<Alert justifyContent='center' textAlign='center' status={(successCode === 2) ? "success" : "error"} variant='subtle'>{codeConfig[successCode.toString()]}</Alert>)}
-      <Box p={4} maxW="400px" mx="auto">
-        <Heading as="h2" mb={6} textAlign="center">
-          Sign Up
-        </Heading>
-        <form onSubmit={handleSubmit}>
-          <FormControl mb={4}>
-            <FormLabel>Username</FormLabel>
-            <Input name="username" value={formData.username} onChange={handleInputChange} />
-          </FormControl>
-          <FormControl mb={4}>
-            <FormLabel>Email</FormLabel>
-            <Input name="email" value={formData.email} onChange={handleInputChange} />
-          </FormControl>
-          <FormControl mb={4}>
-            <FormLabel>School Email</FormLabel>
-            <Input name="school_email" value={formData.school_email} onChange={handleInputChange} />
-          </FormControl>
-          <FormControl mb={4}>
-            <FormLabel>Password</FormLabel>
-            <InputGroup>
-              <Input name="password" type={show ? "text" : "password" } value={formData.password} onChange={handleInputChange} />
-              <InputRightElement width='4.5rem'>
-                <Button h='1.75rem' size='sm' onClick={changeVisibility}>
-                  {show ? 'Hide' : 'Show'}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </FormControl>
-          <FormControl mb={4}>
-            <FormLabel>School</FormLabel>
-            <Select
-              placeholder="Select your school"
-              name="school_id"
-              value={formData.school_id}
-              onChange={handleInputChange}
-              maxH="80px"
-            >
-              {loading ? (
-                <option>Failed to load schools...</option>
-              ) : (
-                Object.values(schools).map((college) => (
-                  <option key={college.name} value={college.id}>
-                    {college.name}
-                  </option>
-                ))
-              )}
-            </Select>
-          </FormControl>
-          <Button type="submit" colorScheme="teal" mb={4} width="100%">
-            Sign Up
-          </Button>
-        </form>
-        <Box textAlign="center">
-          Already have an account?{' '}
-          <ChakraLink color="teal.500" href="/login">
-            Log in here
-          </ChakraLink>
+      <Grid
+        templateColumns="1fr 2fr 1fr" // Three columns with the middle column being twice the width of the side columns
+        gap={6} // Gap between columns
+        p={4} // Padding for the whole grid
+      >
+        <Box p={4}
+          w = "400px"
+          h = "8000px"
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+          bg="white"
+          boxShadow="lg">
         </Box>
-      </Box>
+        <Box p={4} maxW="400px" mx="auto">
+          <Heading as="h2" mb={6} textAlign="center">
+            Sign Up
+          </Heading>
+          <form onSubmit={handleSubmit}>
+            <FormControl mb={4}>
+              <FormLabel>Username</FormLabel>
+              <Input name="username" value={formData.username} onChange={handleInputChange} />
+            </FormControl>
+            <FormControl mb={4}>
+              <FormLabel>Email</FormLabel>
+              <Input name="email" value={formData.email} onChange={handleInputChange} />
+            </FormControl>
+            <FormControl mb={4}>
+              <FormLabel>School Email</FormLabel>
+              <Input name="school_email" value={formData.school_email} onChange={handleInputChange} />
+            </FormControl>
+            <FormControl mb={4}>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input name="password" type={show ? "text" : "password" } value={formData.password} onChange={handleInputChange} />
+                <InputRightElement width='4.5rem'>
+                  <Button h='1.75rem' size='sm' onClick={changeVisibility}>
+                    {show ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <FormControl mb={4}>
+              <FormLabel>School</FormLabel>
+              <Select
+                placeholder="Select your school"
+                name="school_id"
+                value={formData.school_id}
+                onChange={handleInputChange}
+                maxH="80px"
+              >
+                {loading ? (
+                  <option>Failed to load schools...</option>
+                ) : (
+                  Object.values(schools).map((college) => (
+                    <option key={college.name} value={college.id}>
+                      {college.name}
+                    </option>
+                  ))
+                )}
+              </Select>
+            </FormControl>
+            <Button type="submit" colorScheme="teal" mb={4} width="100%">
+              Sign Up
+            </Button>
+          </form>
+          <Box textAlign="center">
+            Already have an account?{' '}
+            <ChakraLink color="teal.500" href="/login">
+              Log in here
+            </ChakraLink>
+          </Box>
+        </Box>
+        <Box p={4}
+          w = "400px"
+          h = "8000px"
+          borderRadius="lg"
+          overflow="hidden"
+          bg="white"
+          boxShadow="md">
+        </Box>
+      </Grid>
     </ChakraProvider>
   );
 
